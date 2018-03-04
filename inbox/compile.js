7ba8816003131fdd64dc167d111d96f77d5d4612
ABCslime <anej.batagelj@gmail.com>
2018-03-04 14:37:32 +0100

    add

--- inbox/compile.js
+++ inbox/compile.js
@@ -7,4 +7,4 @@ const solc = require("solc");
 const inboxPath = path.resolve(__dirname,"concract","inbox1.sol");
 const source  = fs.readFileSync(inboxPath,"utf8");
 
-module.exports = solc.compile(source,1);
+module.exports = solc.compile(source,1).contracts[":inbox"];
